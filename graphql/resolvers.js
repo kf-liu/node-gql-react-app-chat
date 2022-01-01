0363aa6f8c31d2473a2770811821b73cc5a95e08
qiaork-mac21 <1229319295@qq.com>
2022-01-01 23:41:55 +0800

    feat: init

---
+++ graphql/resolvers.js
@@ -0,0 +1,65 @@
+const bcrypt = require('bcryptjs');
+const { UserInputError } = require('apollo-server');
+
+const { User } = require('../models');
+
+module.exports = {
+    Query: {
+        getUsers: async () => {
+            try {
+                const users = await User.findAll();
+                return users;
+            } catch (err) {
+                console.log(err);
+            }
+        },
+    },
+    Mutation: {
+        register: async (_parent, args, _context, _info) => {
+            let { username, email, password, confirmPassword } = args;
+            let errors = {};
+
+            try {
+                // Validate input data
+                if (username.trim() === '') errors.username = 'Username must not be empty';
+                if (email.trim() === '') errors.email = 'Email must not be empty';
+                if (password.trim() === '') errors.password = 'Password must not be empty';
+                if (confirmPassword.trim() === '') errors.confirmPassword = 'ConfirmPassword must not be empty';
+
+                if (password !== confirmPassword) errors.confirmPassword = 'Password must match';
+
+                // // Check if username / email exists
+                // const userByUsername = await User.findOne({ where: { username } });
+                // const userByEmail = await User.findOne({ where: { email } });
+
+                // if (userByUsername) errors.username = 'Username is taken';
+                // if (userByEmail) errors.email = 'Email is taken';
+
+                if (Object.keys(errors).length > 0) {
+                    throw errors;
+                }
+
+                // Hash password
+                password = await bcrypt.hash(password, 6);
+
+                // Create user
+                const user = await User.create({
+                    username,
+                    email,
+                    password,
+                });
+
+                // Return user
+                return user;
+            } catch (err) {
+                console.log(err);
+                if (err.name === 'SequelizeUniqueConstraintError') {
+                    err.errors.forEach(e => errors[e.path] = `${e.path} is already taken`);
+                } else if (err.name === 'SequelizeValidationError') {
+                    err.errors.forEach(e => errors[e.path] = e.message);
+                }
+                throw new UserInputError('Bad Input', { errors });
+            }
+        }
+    }
+};
