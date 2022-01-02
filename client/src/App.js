06c69c592d9d240d7155854382add169490aaca3
qiaork-mac21 <1229319295@qq.com>
2022-01-02 14:56:02 +0800

    feat: init client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+import Register from './pages/Register';
+import './App.scss';
+
+function App() {
+
+  return (
+    <Container className="pt-5">
+      <Register />
+    </Container>
+  );
+}
+
+export default App;
